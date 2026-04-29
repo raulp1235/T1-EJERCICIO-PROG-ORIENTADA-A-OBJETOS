634206bd23686fe1bef4925f1774e0290a7e6f5a
raulp1235 <raulpinedo681@gmail.com>
2026-04-28 21:27:43 -0500

    ESTRUCTURA

---
+++ T1OBJETOS/src/main/java/com/mycompany/t1objetos/T1OBJETOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t1objetos;
+
+/**
+ *
+ * @author reilx
+ */
+public class T1OBJETOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
